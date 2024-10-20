d595b5ab88af7f6357a863fe016d49be5673ec3d
Sadamdi <sultanadamr@gmail.com>
2024-10-20 21:50:39 +0700

    Upload initial files from testing project

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,81 +1,152 @@
 import 'package:flutter/material.dart';
 
-class RecipeDetailPage extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String ingredients;
-  final String instructions;
+import 'recipe_data.dart';
 
-  const RecipeDetailPage({
-    required this.imageUrl,
-    required this.title,
-    required this.ingredients,
-    required this.instructions,
-    super.key,
-  });
+class DetailPage extends StatefulWidget {
+  final int recipeId;
+
+  const DetailPage({Key? key, required this.recipeId}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  late Future<dynamic> _recipe;
+
+  @override
+  void initState() {
+    super.initState();
+    _recipe = RecipeData.fetchRecipeById(
+        widget.recipeId); // Mengambil data detail dari API
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-             
-              Center(
-                child: Image.asset(
-                  imageUrl,
-                  width: 300,
-                  height: 200,
-                  fit: BoxFit.cover,
+      // Menghilangkan AppBar biasa
+      body: FutureBuilder<dynamic>(
+        future: _recipe,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final recipe = snapshot.data!;
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 250,
+                  pinned: true, // AppBar tetap menempel di atas
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(recipe['name']),
+                    background: Image.network(
+                      recipe['image'] ?? 'https://via.placeholder.com/150',
+                      fit: BoxFit.cover,
+                    ),
+                    collapseMode: CollapseMode
+                        .parallax, // Mengaktifkan parallax untuk scroll
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Judul resep
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                SliverList(
+                  delegate: SliverChildListDelegate([
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.timer),
+                                  const SizedBox(width: 4),
+                                  Text('${recipe['prepTimeMinutes']} min'),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.local_fire_department),
+                                  const SizedBox(width: 4),
+                                  Text('${recipe['caloriesPerServing']} Kcal'),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          Divider(color: Colors.teal), // Garis hijau
+                          const Text(
+                            'Ingredients',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal, // Warna hijau
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(
+                                  12.0), // Tambahkan padding untuk spasi antar baris
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  for (var ingredient in recipe['ingredients'])
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical:
+                                              4.0), // Tambahkan space antar baris
+                                      child: Text('- $ingredient'),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Divider(color: Colors.teal), // Garis hijau
+                          const Text(
+                            'Instructions',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal, // Warna hijau
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(
+                                  12.0), // Tambahkan padding untuk spasi antar baris
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  for (int i = 0;
+                                      i < recipe['instructions'].length;
+                                      i++)
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical:
+                                              8.0), // Tambahkan jarak antar baris
+                                      child: Text(
+                                          '${i + 1}. ${recipe['instructions'][i]}'),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
                 ),
-              ),
-              const SizedBox(height: 20),
-              
-              const Text(
-                'Ingredients:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                ingredients,
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-            
-              const Text(
-                'Instructions:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                instructions,
-                style: const TextStyle(fontSize: 16),
-              ),
-            ],
-          ),
-        ),
+              ],
+            );
+          } else {
+            return const Center(child: Text('Recipe not found.'));
+          }
+        },
       ),
     );
   }
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,149 +1,165 @@
 import 'package:flutter/material.dart';
+
 import 'detail_page.dart';
-import 'recipe_data.dart'; // Import data resep dari file terpisah
+import 'recipe_data.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List<Map<String, String>> filteredRecipes = [];
-  String searchQuery = '';
+  late Future<List<dynamic>> _recipes;
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    filteredRecipes = recipes;
+    _recipes = RecipeData.fetchRecipes(); // Mengambil semua data resep awalnya
   }
 
-  void _filterRecipes(String query) {
+  // Fungsi untuk melakukan pencarian berdasarkan query
+  void _searchRecipes(String query) {
     setState(() {
-      searchQuery = query;
-      if (searchQuery.isEmpty) {
-        filteredRecipes = recipes;
-      } else {
-        filteredRecipes = recipes
-            .where((recipe) => recipe['title']!
-                .toLowerCase()
-                .contains(searchQuery.toLowerCase()))
-            .toList();
-      }
+      _recipes = RecipeData.searchRecipes(query); // Pencarian langsung di API
     });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose(); // Membersihkan controller
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search'),
-        centerTitle: true,
+        title: const Text('Search Recipes'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'What is in your kitchen?',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              onChanged: _filterRecipes,
-              decoration: InputDecoration(
-                hintText: 'Type your ingredients',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'What is in your kitchen?',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                prefixIcon: const Icon(Icons.search),
-              ),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Type your ingredients',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onChanged: (query) {
+                    _searchRecipes(
+                        query); // Panggil pencarian saat user mengetik
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: filteredRecipes.length,
-                itemBuilder: (context, index) {
-                  final recipe = filteredRecipes[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => RecipeDetailPage(
-                            imageUrl: recipe['imageUrl']!,
-                            title: recipe['title']!,
-                            ingredients: recipe['ingredients']!,
-                            instructions: recipe['instructions']!,
+          ),
+          Expanded(
+            child: FutureBuilder<List<dynamic>>(
+              future: _recipes,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  final recipes = snapshot.data!;
+                  return GridView.builder(
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      childAspectRatio:
+                          3 / 4, // Menambah rasio untuk memperbesar card
+                    ),
+                    itemCount: recipes.length,
+                    itemBuilder: (context, index) {
+                      final recipe = recipes[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailPage(recipeId: recipe['id']),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.vertical(
+                                      top: Radius.circular(16)),
+                                  child: Image.network(
+                                    recipe['image'] ??
+                                        'https://via.placeholder.com/150',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      recipe['name'],
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    const Text('Best Recipe'),
+                                    const SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                            '${recipe['caloriesPerServing']?.toString() ?? 'N/A'} Kcal'),
+                                        Text(
+                                            '${recipe['prepTimeMinutes']?.toString() ?? 'N/A'} min'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       );
                     },
-                    child: RecipeCard(
-                      imageUrl: recipe['imageUrl']!,
-                      title: recipe['title']!,
-                      calories: recipe['calories']!,
-                      time: recipe['time']!,
-                    ),
                   );
-                },
-              ),
+                } else {
+                  return const Center(child: Text('No recipes found.'));
+                }
+              },
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class RecipeCard extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String calories;
-  final String time;
-
-  const RecipeCard({
-    Key? key,
-    required this.imageUrl,
-    required this.title,
-    required this.calories,
-    required this.time,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      elevation: 2,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: AssetImage(imageUrl),
-          radius: 30,
-        ),
-        title: Text(
-          title,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
           ),
-        ),
-        subtitle: Row(
-          children: [
-            Text(calories),
-            const SizedBox(width: 10),
-            const Icon(Icons.schedule, size: 16),
-            const SizedBox(width: 5),
-            Text(time),
-          ],
-        ),
+        ],
       ),
     );
   }
